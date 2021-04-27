73af1852fb3e8ff7157b10f27c80340ddea66705
Mahalinoro Razafimanjato <m.razafiman@alustudent.com>
2021-04-27 13:37:17 +0300

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,190 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header class="header-pos bg-white">
+      <div class="full-width row justify-between  items-center content-center">
+        <div class="div-ws">
+          <img class="logo-size" src="~/assets/logo.svg" />
+        </div>
+
+        <div class="overflow: auto">
+          <q-btn
+            unelevated
+            no-caps
+            color="primary"
+            class="sairar-font primary-btn q-px-sm"
+            label="Sign In / Sign Up"
+            @click="dialog = true"
+          />
+        </div>
+      </div>
+    </q-header>
+
+    <q-page-container class="container-pos">
+      <router-view />
+    </q-page-container>
+
+    <q-dialog v-model="dialog">
+      <q-card style="max-width:400px">
+        <q-card-section class="row items-center q-pb-none">
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-selection class="column no-wrap flex-center">
+          <q-spinner-hourglass
+          color="primary"
+          size="2em"
+        />
+        </q-card-selection>
+
+        <q-card-section class="font-body q-pa-lg text-center">
+          Feature still in construction! Thank you for your patience!
+          You will hear from us soon when the new feature will be available.
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      dialog: false,
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.sairar-font {
+  font-family: "Saira Semi Condensed", sans-serif;
+  font-weight: 400;
+}
+
+.sairar-font-md {
+  font-family: "Saira Semi Condensed", sans-serif;
+  font-weight: 500;
+}
+
+.font-page {
+  font-family: "Saira", sans-serif;
+  font-weight: 400;
+  color: $primary;
+}
+
+.font-body {
+  font-family: "Saira", sans-serif;
+  font-weight: 300;
+  color: $dark;
+}
+
+@media only screen and (max-width: 600px) {
+  .primary-btn {
+    border-radius: 20px;
+    opacity: 0.8;
+    font-size: 10px;
+  }
+
+  .header-pos {
+    padding: 2% 10% 2% 10%;
+  }
+
+  .container-pos {
+    padding-left: 10%;
+    padding-right: 10%;
+  }
+
+  .resume {
+    width: 100%;
+  }
+
+  .font-body-size {
+    font-size: 20px;
+  }
+
+  .font-body-title {
+    font-size: 10px;
+  }
+
+  .body-div {
+    width: 90%;
+  }
+
+  .font-caption {
+    font-family: "Open Sans", sans-serif;
+    font-weight: 300;
+    font-size: 10px;
+    color: $dark;
+  }
+
+  .center {
+    margin: auto;
+    width: 80%;
+    text-align: center;
+  }
+
+  .resume-pad {
+    margin-top: 50px;
+    margin-bottom: 10px;
+  }
+
+  .logo-size {
+    width: 100%;
+  }
+
+  .div-ws {
+    width: 50%;
+  }
+}
+
+@media only screen and (min-width: 600px) {
+  .primary-btn {
+    border-radius: 20px;
+    opacity: 0.8;
+    font-size: 15px;
+  }
+
+  .header-pos {
+    padding: 2% 10% 2% 10%;
+  }
+
+  .container-pos {
+    padding-left: 10%;
+    padding-right: 10%;
+  }
+
+  .resume {
+    width: 90%;
+  }
+
+  .font-body-size {
+    font-size: 30px;
+  }
+
+  .div-w {
+    width: 50%;
+  }
+
+  .body-div {
+    width: 80%;
+  }
+
+  .font-caption {
+    font-family: "Open Sans", sans-serif;
+    font-weight: 300;
+    font-size: 15px;
+    color: $dark;
+  }
+
+  .font-body-title {
+    font-size: 15px;
+  }
+
+  .ele-center {
+    margin: auto;
+    width: 50%;
+  }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-page>
+    <div class="full-width row inline wrap justify-start q-pt-lg">
+      <div class="div-w ele-center">
+        <div class="text-justify font-body-size body-div">
+          <p class="font-body">
+            <span class="font-page">Drop Your Resume</span> is a free online
+            storage platform to manage your CV/Resume in one place.
+          </p>
+        </div>
+
+        <div>
+          <q-list class="q-px-md q-pt-xs q-pb-md" style="max-width: 450px">
+            <q-item class="q-mb-sm"
+            v-for="item in listItem"
+            :key="item.icon"
+            >
+              <q-item-section top avatar>
+                <q-avatar
+                  style="opacity: 0.8"
+                  color="primary"
+                  text-color="white"
+                  rounded
+                  :icon="item.icon"
+                />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="font-page text-justify font-body-title">{{ item.title }}</q-item-label>
+                <q-item-label caption class="font-caption text-justify"
+                  >{{ item.description }}</q-item-label
+                >
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+
+        <div class="ele-center center">
+          <q-btn unelevated no-caps color="primary" @click="dialog = true" class="sairar-font-md primary-btn q-px-md" label="Get Started" />
+        </div>
+      </div>
+
+      <div class="div-w ele-center resume-pad center">
+        <img class="resume" src="~/assets/resume.svg" />
+      </div>
+    </div>
+
+    <q-dialog v-model="dialog">
+      <q-card style="max-width:400px">
+        <q-card-section class="row items-center q-pb-none">
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-selection class="column no-wrap flex-center">
+          <q-spinner-hourglass
+          color="primary"
+          size="2em"
+        />
+        </q-card-selection>
+
+        <q-card-section class="font-body q-pa-lg text-center">
+          Feature still in construction! Thank you for your patience!
+          You will hear from us soon when the new feature will be available.
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  
+  data() {
+    return {
+      dialog: false,
+      listItem: [
+        {
+          icon: "folder",
+          title: "Upload & Manage",
+          description:"Upload your files easily by drag & drop. Manage your stored files in one place."
+        },
+
+        {
+          icon: "share",
+          title: "Share  to the World",
+          description:"Each file uploaded contain a custom link. You can share your CV/Resume on every platform."
+        },
+
+        {
+          icon: "security",
+          title: "High Security & Privacy",
+          description:"Your personal data is secured and encrypted as privacy is essential to us."
+        },
+      ]
+    };
+  }
+};
+</script>
