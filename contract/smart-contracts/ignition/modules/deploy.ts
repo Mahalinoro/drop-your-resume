283fc5f0a9ff2df03392f8793be6bd27f4ed1dc5
Mahalinoro <mrazafimanjato@gmail.com>
2025-11-14 16:45:37 +0900

    added ResumeStorage.sol contract

--- contract/smart-contracts/ignition/modules/deploy.ts
+++
@@ -1,6 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("ResumeStorageModule", (m) => {
-  const resumeStorage = m.contract("ResumeStorage");
-  return { resumeStorage };
-});
